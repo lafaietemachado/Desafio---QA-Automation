c9ac3024883f0bc84ef300a96202adda8344dc86
Lafaiete Machado <lafaiete.machado@gmail.com>
2025-09-02 10:28:50 -0300

    feat: implementar relatórios estruturados com cypress-mochawesome-reporter

--- cypress.config.js
+++ cypress.config.js
@@ -4,10 +4,19 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://demoqa.com',
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on)
     },
     specPattern: 'cypress/e2e/**/*.cy.js',
     supportFile: 'cypress/support/e2e.js',
     pageLoadTimeout: 120000,
-    defaultCommandTimeout: 10000
+    defaultCommandTimeout: 10000,
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+      charts: true,
+      reportPageTitle: 'Cypress Test Results',
+      embeddedScreenshots: true,
+      inlineAssets: true,
+      saveAllAttempts: false
+    }
   },
 })
